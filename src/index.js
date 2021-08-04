daef6f3abc46b4a771ab5d36d557686550c717f1
Wayne Fan <57977052+Fanbruary@users.noreply.github.com>
2021-08-04 16:29:36 +1200

    finish the placing ablility of O

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ class Board extends React.Component {
     super(props);
     this.state ={
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -35,8 +36,13 @@ class Board extends React.Component {
   **/
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+    
+
   }
 
   /* 
@@ -52,7 +58,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = this.state.xIsNext ?'Next player: X': 'Next player: O';
 
     return (
       <div>
